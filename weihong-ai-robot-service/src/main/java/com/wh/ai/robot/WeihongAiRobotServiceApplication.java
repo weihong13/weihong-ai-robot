fa082631dd5d281801ecf7897fc8aeee6a90059b
guohongwei <weihong010925@163.com>
2025-10-13 19:08:14 +0800

    feat(AI-Robot):构建AI机器人项目 1、构建AI机器人项目后端 2、SpringBoot3.x整合SpringAI 3、对接DeepSeek实现问答效果（流式对话） 4、对接DeepSeek-R1模型（实现推理效果）

---
+++ weihong-ai-robot-service/src/main/java/com/wh/ai/robot/WeihongAiRobotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wh.ai.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeihongAiRobotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeihongAiRobotServiceApplication.class, args);
+    }
+
+}
