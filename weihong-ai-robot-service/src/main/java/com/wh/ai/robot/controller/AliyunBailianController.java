6cde9aab7c263ab93102513590468867e4137a5c
guohongwei <weihong010925@163.com>
2025-10-14 15:00:09 +0800

    feat(智谱、OpenAI、阿里云百炼)：新增智谱、OpenAI、阿里云百炼大模型 1、接入智谱AI 2、接入OpenAI 3、接入阿里云百炼大模型平台

---
+++ weihong-ai-robot-service/src/main/java/com/wh/ai/robot/controller/AliyunBailianController.java
@@ -0,0 +1,60 @@
+package com.wh.ai.robot.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
+
+/**
+ * @Author: ghw
+ * @Date: 2025/10/14 14:55
+ * @Version: v1.0.0
+ * @Description: 对接 阿里云百炼
+ **/
+@RestController
+@RequestMapping("/v6/ai")
+public class AliyunBailianController {
+
+    @Resource
+    private OpenAiChatModel chatModel;
+
+    /**
+     * 普通对话
+     * @param message
+     * @return
+     */
+    @GetMapping("/generate")
+    public String generate(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 一次性返回结果
+        return chatModel.call(message);
+    }
+
+    /**
+     * 流式对话
+     * @param message
+     * @return
+     */
+    @GetMapping(value = "/generateStream", produces = "text/html;charset=utf-8")
+    public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
+        // 构建提示词
+        Prompt prompt = new Prompt(new UserMessage(message));
+
+        // 流式输出
+        return chatModel.stream(prompt)
+                .mapNotNull(chatResponse -> {
+                    Generation generation = chatResponse.getResult();
+                    return Objects.nonNull(generation) ? generation.getOutput().getText() : null;
+                });
+
+    }
+
+}
+
