d74232868a018aa033c3873911e3df00ca93913f
guohongwei <weihong010925@163.com>
2025-10-14 15:59:56 +0800

    feat(Service):SSE服务端单向推送消息、跨域配置

--- weihong-ai-robot-service/src/main/java/com/wh/ai/robot/controller/AliyunBailianController.java
+++ weihong-ai-robot-service/src/main/java/com/wh/ai/robot/controller/AliyunBailianController.java
@@ -5,6 +5,7 @@ import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -42,7 +43,7 @@ public class AliyunBailianController {
      * @param message
      * @return
      */
-    @GetMapping(value = "/generateStream", produces = "text/html;charset=utf-8")
+    @GetMapping(value = "/generateStream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> generateStream(@RequestParam(value = "message", defaultValue = "你是谁？") String message) {
         // 构建提示词
         Prompt prompt = new Prompt(new UserMessage(message));
