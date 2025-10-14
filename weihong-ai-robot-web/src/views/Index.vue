d5fbdcb07643ee88e91fe3e2be9433dc086cf78d
guohongwei <weihong010925@163.com>
2025-10-14 15:21:44 +0800

    chore(AI-Robot)：AI机器人前端项目构建

---
+++ weihong-ai-robot-web/src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h1 class="text-3xl font-bold bg-yellow-500">
+        AI 机器人 - 首页
+      </h1>
+      <a-button type="primary">发送</a-button>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  </style>
+  
