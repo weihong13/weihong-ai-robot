f10a8f7a0f49b4e5ee559d0d33e249ae07721f3c
guohongwei <weihong010925@163.com>
2025-10-22 14:12:25 +0800

    chore(project/service)：AI机器人后端项目构建 1、删除案例代码、删除无用依赖 2、修改ChatClientConfig配置类 3、修改配置项、增加多环境配置（开发、生产环境）

--- project/weihong-ai-robot-service/src/main/java/com/wh/ai/robot/config/ChatClientConfig.java
+++ project/weihong-ai-robot-service/src/main/java/com/wh/ai/robot/config/ChatClientConfig.java
@@ -1,37 +1,29 @@
 package com.wh.ai.robot.config;
 
-import com.wh.ai.robot.advisor.MyLoggerAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.deepseek.DeepSeekChatModel;
-import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 /**
- * @Author: ghw
- * @Date: 2025/10/14 10:19
+ * @Author: GHW
+ * @Date: 2025/10/22 14:03
  * @Version: v1.0.0
- * @Description: TODO
+ * @Description: ChatClient 配置类
  **/
 @Configuration
 public class ChatClientConfig {
 
     /**
      * 初始化 ChatClient 客户端
-     * @param chatModel 依赖注入 starter 自动装配的 DeepSeekChatModel 实例，它是 ChatModel 接口的具体实现类，表示底层对接的 DeepSeek 大语言模型；
+     * @param chatModel
      * @return
      */
     @Bean
-    public ChatClient chatClient(DeepSeekChatModel chatModel, ChatMemory chatMemory, ToolCallbackProvider tools) {
+    public ChatClient chatClient(OpenAiChatModel chatModel) {
         return ChatClient.builder(chatModel)
-                .defaultToolCallbacks(tools)
-//                .defaultSystem("请你扮演一位市政院的AI机器人")
-                .defaultAdvisors(new SimpleLoggerAdvisor(),
-//                                    new MyLoggerAdvisor(), // 添加 Spring AI 内置的日志记录功能
-                        MessageChatMemoryAdvisor.builder(chatMemory).build()
-                                )
-                .build();   // 使用 ChatClient 的建造者模式（Builder），将 DeepSeekChatModel 实例作为底层模型绑定到 ChatClient 客户端
+                .build();
     }
 }
+
+
