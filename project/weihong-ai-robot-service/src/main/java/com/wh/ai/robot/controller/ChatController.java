da4d15ccae581308800b16ba8425ddb5bcf77abb
guohongwei <weihong010925@163.com>
2025-10-22 16:07:01 +0800

    feat(project/service)：整合log4j2日志 1、添加依赖 2、添加日志配置文件 3、设置日志配置文件

--- project/weihong-ai-robot-service/src/main/java/com/wh/ai/robot/controller/ChatController.java
+++ project/weihong-ai-robot-service/src/main/java/com/wh/ai/robot/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.wh.ai.robot.controller;
 
 import com.wh.ai.robot.aspect.ApiOperationLog;
 import com.wh.ai.robot.utils.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     @PostMapping("/new")
     @ApiOperationLog(description = "新建对话")
     public Response<?> newChat() {
-        int i = 1/0;
+        log.trace("这是一个 TRACE 级别日志");
+        log.debug("这是一个 DEBUG 级别日志");
+        log.info("这是一个 INFO 级别日志");
+        log.warn("这是一个 WARN 级别日志");
+        log.error("这是一个 ERROR 级别日志");
+
+        // 模拟一个运行时错误（分母不能为 0）
+        int i = 1 / 0;
+
         return Response.success();
     }
 
