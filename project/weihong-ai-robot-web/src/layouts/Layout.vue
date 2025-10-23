d32e09a50b3e9fecc04103ed266b2c19c9a1272d
guohongwei <weihong010925@163.com>
2025-10-23 15:32:40 +0800

    feat(project/web)：页面Layout基础布局 1、调整界面为左右布局

---
+++ project/weihong-ai-robot-web/src/layouts/Layout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="h-screen flex overflow-hidden overflow-x-hidden">
+        <!-- 左边栏 -->
+        <div class="w-64 bg-red-200 border-r border-gray-200">
+            左侧栏
+        </div>
+        
+        <!-- 主内容区域 -->
+        <div class="bg-green-200 flex-1">
+            主内容区域
+        </div>
+    </div>
+</template>
